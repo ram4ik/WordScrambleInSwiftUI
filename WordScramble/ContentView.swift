9d5f758312f0ef5f5d6dcf44dfa0d2eccc0345db
ram4ik <ram4ik@hotmail.com>
2023-10-24 20:22:33 +0300

    working with strings

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -24,6 +24,32 @@ struct ContentView: View {
             }
         }
     }
+    
+    func testStrings() {
+        let input = "a b c"
+        let letters = input.components(separatedBy: " ")
+    }
+    
+    func testStringsOther() {
+        let input = """
+        a 
+        b
+        c
+        """
+        let letters = input.components(separatedBy: "\n")
+        let letter = letters.randomElement()
+        let trimmed = letter?.trimmingCharacters(in: .whitespacesAndNewlines)
+    }
+    
+    func checkMissSpell() {
+        let word = "swift"
+        let checker = UITextChecker()
+        
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        
+        let allGood = misspelledRange.location == NSNotFound
+    }
 }
 
 #Preview {
