316b28d09ddb0cb8d6659a72b9779ef733630033
ram4ik <ram4ik@hotmail.com>
2023-10-24 21:10:23 +0300

    runnning code when our app launches

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -32,6 +32,7 @@ struct ContentView: View {
             }
             .navigationTitle(rootWord)
             .onSubmit(addNewWord)
+            .onAppear(perform: startGame)
         }
     }
     
@@ -44,6 +45,18 @@ struct ContentView: View {
             userWords.insert(answer, at: 0)
             newWord = ""
         }
+    }
+    
+    func startGame() {
+        if let startWorldsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
+            if let startWords = try? String(contentsOf: startWorldsURL) {
+                let allWords = startWords.components(separatedBy: "\n")
+                rootWord = allWords.randomElement() ?? "silkworm"
+                return
+            }
+        }
+        
+        fatalError("Could not load start.txt from bundle.")
     }
 }
 
